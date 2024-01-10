988525bb3ac5fec3fd5172fbedd99da16148a78a
HakdanDemirci <156220082+HakdanDemirci@users.noreply.github.com>
2024-01-10 19:08:21 +0300

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,46 @@
+sayi1=100
+sayi1=200
+print(sayi1)
+
+yas=10
+yas=100
+print(yas)
+
+adi,yasi,sehir = ("ali",15,"arabic")
+print(adi,yasi,sehir)
+
+kelime = "merhaba/kanka"
+print(kelime)
+
+x,y,z,= (4,5,6)
+print(x,y,z,)
+
+sayilar ="10"
+sayilar1="20"
+print(sayilar+sayilar1)
+
+maas=17002
+vergi=0.20
+
+print(maas - ( maas * vergi))
+
+isim= "ahmet"
+print ("merhaba" " "  + isim +  " hoşgelmişen ağam")
+
+print (5==5)
+
+rakam =-7
+print("mutlak değer", abs(rakam))
+
+us = pow(2,3)
+print("sayının üssü",us)
+
+from math import sqrt,ceil,floor
+Sayi= 25
+karekok=sqrt(Sayi)
+print ("sayının karekökü", karekok)
+
+ondalık =  10.25
+yukarı=ceil(ondalık)
+asagi= floor(ondalık)
+print("yukarı: ", yukarı  , "aşağı: " , asagi)
