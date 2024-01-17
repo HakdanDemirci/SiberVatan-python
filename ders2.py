4f337bff171e23bbec9bffde5e39d19574d60b7e
HakdanDemirci <156220082+HakdanDemirci@users.noreply.github.com>
2024-01-17 18:58:02 +0300

    Add files via upload

---
+++ ders2.py
@@ -0,0 +1,33 @@
+ondalik1=2.14
+ondalik2=3.89
+
+sayi1=45
+sayi2=-1
+print(type(sayi2))
+
+sayi3=1.5
+sayi4=-5.5
+print(type(sayi3))
+
+dogru_mu=True
+yanlis_mı=False
+print(type(dogru_mu))
+
+sayi_float= float(sayi1)
+print("int to float",sayi_float)
+
+sayi_int=int(ondalik1)
+print("float to int", sayi_int)
+
+sayi_bool=int(dogru_mu)
+print("bool to int",sayi_bool)
+
+print(type(sayi_bool))
+
+#kullanici_adi=input("ad gir:")
+#print("hoşgeldiniz",kullanici_adi)
+
+girilen_sayi1=int(input("1.sayi gir:"))
+girilen_sayi2=int(input("2.sayi gir:"))
+print(girilen_sayi1+girilen_sayi2)
+
